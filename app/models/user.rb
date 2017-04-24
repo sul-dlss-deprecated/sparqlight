f9ec85f05369391814bbe5339125c674d5c974e6
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-04-24 13:32:36 -0700

    rails5 update -> app/models/

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
 
   if Blacklight::Utils.needs_attr_accessible?
-
     attr_accessible :email, :password, :password_confirmation
   end
-# Connects this user object to Blacklights Bookmarks. 
+  # Connects this user object to Blacklights Bookmarks.
   include Blacklight::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
